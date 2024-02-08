c693a9f852abb09e11055b23d4e6eeeb9f51e045
zhu2qian1 <zhu2qian1@gmail.com>
2024-02-09 00:10:25 +0900

    move dateutil

--- app/src/main/java/jsh/utils/DateUtil.java
+++ app/src/main/java/jsh/utils/DateUtil.java
@@ -1,4 +1,4 @@
-package jsh.example;
+package jsh.utils;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
--- app/src/test/java/jsh/utils/DateUtilTest.java
+++ app/src/test/java/jsh/utils/DateUtilTest.java
@@ -1,4 +1,4 @@
-package jsh.example;
+package jsh.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
