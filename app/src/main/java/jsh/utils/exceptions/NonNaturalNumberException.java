5092965165426d41c77fc542470b5f83ff07c8a3
zhu2qian1 <zhu2qian1@gmail.com>
2024-02-11 21:48:58 +0900

    add custom exception

---
+++ app/src/main/java/jsh/utils/exceptions/NonNaturalNumberException.java
@@ -0,0 +1,23 @@
+package jsh.utils.exceptions;
+
+/**
+ * Thrown to indicate that a method has been passed non-natural number as an argument while
+ * expecting natural number.
+ */
+public class NonNaturalNumberException extends IllegalArgumentException {
+    public NonNaturalNumberException() {
+        super();
+    }
+
+    public NonNaturalNumberException(String message) {
+        super(message);
+    }
+
+    public NonNaturalNumberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NonNaturalNumberException(Throwable cause) {
+        super(cause);
+    }
+}
