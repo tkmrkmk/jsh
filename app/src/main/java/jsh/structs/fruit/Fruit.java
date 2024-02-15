3a51c218d250ea1452474d9d579687f04a2282b3
zhu2qian1 <zhu2qian1@gmail.com>
2024-02-15 22:29:25 +0900

    fix use List instead of Array

--- app/src/main/java/jsh/structs/fruit/Fruit.java
+++ app/src/main/java/jsh/structs/fruit/Fruit.java
@@ -1,18 +1,19 @@
 package jsh.structs.fruit;
 
+import java.util.List;
 import jsh.structs.item.Item;
 import lombok.Getter;
 
 @Getter
 public class Fruit extends Item {
-    protected String[] adjectives;
+    protected List<String> adjectives;
 
     public Fruit(final int price, final String name) {
         super(price, name);
-        this.adjectives = new String[0];
+        this.adjectives = List.of();
     }
 
-    public Fruit(final int price, final String name, final String[] adjectives) {
+    public Fruit(final int price, final String name, final List<String> adjectives) {
         super(price, name);
         this.adjectives = adjectives;
     }
