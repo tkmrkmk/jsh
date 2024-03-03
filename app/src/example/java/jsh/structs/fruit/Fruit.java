9d2a0836d796946ab2d99be48d4755e94348539d
zhu2qian1 <zhu2qian1@gmail.com>
2024-03-03 14:38:29 +0900

    fix - add @NonNull annotation to constructor for better null analysis

--- app/src/example/java/jsh/structs/fruit/Fruit.java
+++ app/src/example/java/jsh/structs/fruit/Fruit.java
@@ -3,12 +3,13 @@ package jsh.structs.fruit;
 import java.util.List;
 import jsh.structs.item.Item;
 import lombok.Getter;
+import lombok.NonNull;
 
 @Getter
 public class Fruit extends Item {
     protected List<String> adjectives;
 
-    public Fruit(final int price, final String name) {
+    public Fruit(final int price, @NonNull final String name) {
         super(price, name);
         this.adjectives = List.of();
     }
