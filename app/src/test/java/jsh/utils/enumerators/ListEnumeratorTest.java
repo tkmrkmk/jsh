dbbf908a7914efa7a0423fee230a39c416869595
zhu2qian1 <zhu2qian1@gmail.com>
2024-03-03 21:34:42 +0900

    add test

--- app/src/test/java/jsh/utils/enumerators/ListEnumeratorTest.java
+++ app/src/test/java/jsh/utils/enumerators/ListEnumeratorTest.java
@@ -9,25 +9,25 @@ import org.junit.jupiter.api.Test;
 import jsh.utils.debugging.DebuggingUtil;
 
 public class ListEnumeratorTest {
-    @Test
-    void ignoreOutsideChange() {
-        final var listTestAgainst = List.of("Hop", "Step", "Jump");
+    private final Function<EnumerationEntry<?>, String> enumerationEntryStringier = e -> {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(e.index).append(" :").append(e.element);
+        return sb.toString();
+    };
 
-        final var variableList = new ArrayList<String>();
+    @Test
+    void ignore_source_list_modification() {
+        final List<String> listTestAgainst = List.of("Hop", "Step", "Jump");
+
+        final ArrayList<String> variableList = new ArrayList<String>();
         for (final var s : new String[] {"Hop", "Step", "Jump"}) {
             variableList.add(s);
         }
 
-        final Function<EnumerationEntry<String>, String> f = e -> {
-            final StringBuilder sb = new StringBuilder();
-            sb.append(e.index).append(' ').append(e.element);
-            return sb.toString();
-        };
-
         final ListEnumerator<String> listIterator1 = new ListEnumerator<>(variableList);
         for (; listIterator1.hasNext();) {
             final var entry = listIterator1.next();
-            DebuggingUtil.printObject(f, entry);
+            DebuggingUtil.printObject(enumerationEntryStringier, entry);
             assertEquals(listTestAgainst.get(entry.index), entry.element);
         }
 
@@ -35,17 +35,27 @@ public class ListEnumeratorTest {
         variableList.add("Another Step");
         variableList.add("Another Jump");
 
-        System.out.println(variableList);
+        System.out.println("" + variableList);
         assertNotEquals(listTestAgainst, variableList);
 
         final ListEnumerator<String> listIterator2 = new ListEnumerator<>(variableList);
         for (; listIterator2.hasNext();) {
             final var entry = listIterator2.next();
-            DebuggingUtil.printObject(f, entry);
+            DebuggingUtil.printObject(enumerationEntryStringier, entry);
         }
 
         System.out.println("listIterator1 " + listIterator1.list);
         System.out.println("listIterator2 " + listIterator2.list);
         assertNotEquals(listIterator1, listIterator2);
+    }
+
+    @Test
+    void iteration_against_empty_list() {
+        final var emptyList = List.of();
+        final ListEnumerator<?> emptyListIterator = new ListEnumerator<>(emptyList);
+        for (; emptyListIterator.hasNext();) {
+            final var entry = emptyListIterator.next();
+            DebuggingUtil.printObject(enumerationEntryStringier, entry);
+        }
     }
 }
