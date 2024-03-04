2990d3ef806ca321489e99bf5150a3bf4edb576c
zhu2qian1 <zhu2qian1@gmail.com>
2024-03-04 16:26:18 +0900

    chore - clean up code

--- app/src/test/java/jsh/util/enumerator/ArrayEnumeratorTest.java
+++ app/src/test/java/jsh/util/enumerator/ArrayEnumeratorTest.java
@@ -1,31 +1,33 @@
 package jsh.util.enumerator;
 
+import java.util.function.Consumer;
 import org.junit.jupiter.api.Test;
 import jsh.util.debugging.DebuggingUtil;
 
 public class ArrayEnumeratorTest {
+
+    private final Consumer<Enumeration<?>> printFunc = elem -> {
+        DebuggingUtil.printObject(ee -> ee.index + ": " + ee.element, elem);
+    };
+
     @Test
     void stream_test() {
         final String[] sourceArray = new String[] {"zero", "one", "two", "three", "four"};
-        new ArrayEnumerator<>(sourceArray).stream().forEach(
-                elem -> DebuggingUtil.printObject(ee -> ee.index + ": " + ee.element, elem));
+        new ArrayEnumerator<>(sourceArray).stream().forEach(printFunc);
     }
 
     @Test
     void stream_against_empty_array() {
         final String[] emptyArray = new String[] {};
-        new ArrayEnumerator<>(emptyArray).stream().forEach(
-                elem -> DebuggingUtil.printObject(ee -> ee.index + ": " + ee.element, elem));
+        new ArrayEnumerator<>(emptyArray).stream().forEach(printFunc);
     }
 
     @Test
     void multiple_times_stream_foreach() {
         final String[] sourceArray = new String[] {"zero", "one", "two", "three", "four"};
         System.out.println("First\n----");
-        new ArrayEnumerator<>(sourceArray).stream().forEach(
-                elem -> DebuggingUtil.printObject(ee -> ee.index + ": " + ee.element, elem));
+        new ArrayEnumerator<>(sourceArray).stream().forEach(printFunc);
         System.out.println("\nSecond\n----");
-        new ArrayEnumerator<>(sourceArray).stream().forEach(
-                elem -> DebuggingUtil.printObject(ee -> ee.index + ": " + ee.element, elem));
+        new ArrayEnumerator<>(sourceArray).stream().forEach(printFunc);
     }
 }
