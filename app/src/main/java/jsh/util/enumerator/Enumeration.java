2659c39985d7d46d4032317f4700e7f77e717fa8
zhu2qian1 <zhu2qian1@gmail.com>
2024-03-17 23:12:17 +0900

    chore

--- app/src/main/java/jsh/util/enumerator/Enumeration.java
+++ app/src/main/java/jsh/util/enumerator/Enumeration.java
@@ -1,10 +1,12 @@
 package jsh.util.enumerator;
 
+import jakarta.annotation.Nonnull;
+
 public class Enumeration<E> {
     public final int index;
     public final E element;
 
-    public Enumeration(final int index, final E element) {
+    public Enumeration(final int index, @Nonnull final E element) {
         this.index = index;
         this.element = element;
     }
