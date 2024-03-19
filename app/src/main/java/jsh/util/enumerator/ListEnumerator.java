7a1bba64170470077e7edd0f6bc504276007cbbc
zhu2qian1 <zhu2qian1@gmail.com>
2024-03-20 01:25:49 +0900

    format

--- app/src/main/java/jsh/util/enumerator/ListEnumerator.java
+++ app/src/main/java/jsh/util/enumerator/ListEnumerator.java
@@ -1,14 +1,16 @@
 package jsh.util.enumerator;
 
 import java.util.List;
+import java.util.Objects;
+import jakarta.annotation.Nonnull;
 
 public class ListEnumerator<E> implements Enumerator<E> {
-    private final List<E> list;
+    @Nonnull private final List<E> list;
     private final int lastIndex;
     private int cursor;
 
-    public ListEnumerator(final List<E> list) {
-        this.list = List.copyOf(list);
+    public ListEnumerator(@Nonnull final List<E> list) {
+        this.list = Objects.requireNonNull(List.copyOf(list));
         this.lastIndex = list.size() - 1;
         this.cursor = -1;
     }
