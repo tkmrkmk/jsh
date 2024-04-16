2aacefa0b7b4af28018fd873f1504fd435bbfb0b
zhu2qian1 <zhu2qian1@gmail.com>
2024-04-16 22:11:00 +0900

    add validation

--- app/src/main/java/jsh/util/validation/ValidationUtil.java
+++ app/src/main/java/jsh/util/validation/ValidationUtil.java
@@ -1,11 +1,13 @@
 package jsh.util.validation;
 
 import java.util.Objects;
+import jakarta.annotation.Nonnull;
 
 public final class ValidationUtil {
     private ValidationUtil() {}
 
-    public static void validateNonNulls(final Object... os) {
+    public static void validateNonNulls(@Nonnull final Object... os) {
+        Objects.requireNonNull(os);
         for (final var o : os) {
             Objects.requireNonNull(o);
         }
