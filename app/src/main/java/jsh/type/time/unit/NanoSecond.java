62efe93cb7ebe40f8d9d47447771f61f3f0278a5
zhu2qian1 <zhu2qian1@gmail.com>
2024-09-23 18:41:52 +0900

    add Nanoseconds

---
+++ app/src/main/java/jsh/type/time/unit/NanoSecond.java
@@ -0,0 +1,72 @@
+package jsh.type.time.unit;
+
+import java.math.BigInteger;
+
+public class NanoSecond implements Comparable<NanoSecond> {
+    private final long value;
+
+    public NanoSecond(final long value) {
+        if (value < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.value = value;
+    }
+
+    public long value() {
+        return this.value;
+    }
+
+    public MilliSecond toMilliSecond() {
+        return new MilliSecond(this.value * 1_000_000);
+    }
+
+    public boolean lt(final NanoSecond o) {
+        return this.value < o.value;
+    }
+
+    public boolean le(final NanoSecond o) {
+        return this.value <= o.value;
+    }
+
+    public boolean gt(final NanoSecond o) {
+        return this.value < o.value;
+    }
+
+    public boolean ge(final NanoSecond o) {
+        return this.value <= o.value;
+    }
+
+    public NanoSecond subtract(final NanoSecond ns) {
+        final long t = this.value;
+        final long a = ns.value;
+
+        // validation
+        final BigInteger bi = new BigInteger(String.valueOf(t));
+        bi.subtract(new BigInteger(String.valueOf(a)));
+        if (bi.compareTo(new BigInteger(String.valueOf(Long.MIN_VALUE))) <= -1) {
+            throw new ArithmeticException();
+        }
+        return new NanoSecond(t - a);
+    }
+
+    public NanoSecond add(final NanoSecond ns) {
+        final long t = this.value;
+        final long a = ns.value;
+
+        // input validation
+        final BigInteger bi = new BigInteger(String.valueOf(a));
+        bi.add(new BigInteger(String.valueOf(t)));
+        if (bi.compareTo(new BigInteger(String.valueOf(Long.MAX_VALUE))) >= 1) {
+            throw new ArithmeticException();
+        }
+
+        return new NanoSecond(t + a);
+    }
+
+    @Override
+    public int compareTo(final NanoSecond anotherNs) {
+        final long t = this.value;
+        final long a = anotherNs.value;
+        return (t < a) ? -1 : ((t == a) ? 0 : 1);
+    }
+}
